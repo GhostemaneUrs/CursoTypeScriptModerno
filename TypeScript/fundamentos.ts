a2026acad8ce4f7e226ccc450d4f59605229a040
Andres Jaramillo <JaramilloG1999@outlook.com>
2021-12-07 01:19:08 -0500

    feat(root): update fundamentos

--- TypeScript/fundamentos.ts
+++ TypeScript/fundamentos.ts
@@ -1,9 +1,10 @@
 /* TypeScript es de tipado estatico, tambien puede ser dinamico, porque el debe detectar que tipo de datos es una variable */
 
+let dato: any = 'Esto es un dato any' //Los tipo any son para variables que no sabemos que tipo de datos son
 let names: string = "Andres Jaramillo"; // string
 let age: number = 23; // number es un tipo de datos
 let work: string = 'Programador'; // string
-let hobbies: string[] = ["Estudiar programación", " escuchar musica", " jugar Fortnite"];   // Arreglo de strings
+let hobbies: string[] = ["Estudiar programación", " escuchar musica", " jugar Fortnite"]; // Arreglo de strings
 const PersonStatus: boolean = true; //constante
 
 console.log(`Mi nombre es: ${names}, tengo ${age} años y soy ${work}, me gusta hacer en mi dia a dia: ${hobbies}`);
