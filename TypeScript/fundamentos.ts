a15d09542dc05e8b1e6ed123dfacba85e69196e0
Andres Jaramillo <JaramilloG1999@outlook.com>
2021-12-07 01:24:12 -0500

    feat(root): update fundamentos

--- TypeScript/fundamentos.ts
+++ TypeScript/fundamentos.ts
@@ -1,6 +1,7 @@
-/* TypeScript es de tipado estatico, tambien puede ser dinamico, porque el debe detectar que tipo de datos es una variable */
+/* TypeScript es de tipado estatico, tambien puede ser dinamico, porque el debe detectar que tipo de datos es una variable, typeScript se comprueba en tiempo de compilacion, no en ejecucion */
 
 let dato: any = 'Esto es un dato any' //Los tipo any son para variables que no sabemos que tipo de datos son
+let datoRandom: unknown = 'Esto es un dato unknown' //Los tipo unknown son para variables que no sabemos que tipo de datos son, pero no podemos acceder a sus propiedades, ni construir una instancia de ellas
 let names: string = "Andres Jaramillo"; // string
 let age: number = 23; // number es un tipo de datos
 let work: string = 'Programador'; // string
@@ -8,4 +9,6 @@ let hobbies: string[] = ["Estudiar programación", " escuchar musica", " jugar F
 const PersonStatus: boolean = true; //constante
 
 console.log(`Mi nombre es: ${names}, tengo ${age} años y soy ${work}, me gusta hacer en mi dia a dia: ${hobbies}`);
+
+
 
